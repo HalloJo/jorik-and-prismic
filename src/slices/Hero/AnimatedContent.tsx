b194a9ddcfaba8826f189c129f931c7a4ceebd08
Jorik van Ruiswijk <jorikvanruiswijk@gmail.com>
2024-10-31 14:06:50 +0100

    Install and add GSAP animations to Hero

---
+++ src/slices/Hero/AnimatedContent.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import ButtonLink from "@/components/ButtonLink";
+import Typography from "@/components/Typography";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const AnimatedContent = ({ slice }: { slice: Content.HeroSlice }) => {
+  const container = useRef(null);
+  gsap.registerPlugin(useGSAP);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+
+      tl.fromTo(".hero__heading", { y: 20 }, { y: 0, opacity: 1, duration: 1 });
+      tl.fromTo(
+        ".hero__description",
+        { y: 20 },
+        { y: 0, opacity: 1, duration: 0.75 },
+        "-=0.6",
+      );
+      tl.fromTo(
+        ".hero__button",
+        { y: 20 },
+        { y: 0, opacity: 1, duration: 0.5 },
+        "-=0.3",
+      );
+      tl.fromTo(".hero__image", { y: 20 }, { y: 0, opacity: 1, duration: 0.5 });
+    },
+    { scope: container },
+  );
+
+  return (
+    <div ref={container}>
+      <div
+        className={`flex ${slice.variation === "reverse" ? "flex-row-reverse" : "flex-row"}`}
+      >
+        <div className="flex-1">
+          <Typography
+            type="h1"
+            field={slice.primary.primary_heading}
+            className="hero__heading text-orange-500 opacity-0"
+          />
+          {isFilled.richText(slice.primary.description) && (
+            <div className="hero__description opacity-0">
+              <PrismicRichText field={slice.primary.description} />
+            </div>
+          )}
+        </div>
+        <div className="hero__button flex-1 opacity-0">
+          <ButtonLink field={slice.primary.button_link}>
+            {slice.primary.button_label}
+          </ButtonLink>
+        </div>
+      </div>
+      <div className="hero__image opacity-0">
+        <PrismicNextImage field={slice.primary.image} />
+        <p className="font-bold text-red-600">{slice.primary.text_field}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedContent;
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -3,6 +3,7 @@ import Typography from "@/components/Typography";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import AnimatedContent from "./AnimatedContent";
 
 /**
  * Props for `Hero`.
@@ -19,30 +20,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="container bg-white"
     >
-      <div
-        className={`flex ${slice.variation === "reverse" ? "flex-row-reverse" : "flex-row"}`}
-      >
-        <div className="flex-1">
-          <h1 className="text-3xl font-extrabold">
-            {slice.primary.primary_heading}
-          </h1>
-          <Typography
-            type="h1"
-            field={slice.primary.primary_heading}
-            className="text-orange-500"
-          />
-          <PrismicRichText field={slice.primary.description} />
-        </div>
-        <div className="flex-1">
-          <ButtonLink field={slice.primary.button_link}>
-            {slice.primary.button_label}
-          </ButtonLink>
-        </div>
-      </div>
-      <div>
-        <PrismicNextImage field={slice.primary.image} />
-        <p className="font-bold text-red-600">{slice.primary.text_field}</p>
-      </div>
+      <AnimatedContent slice={slice} />
     </section>
   );
 };
